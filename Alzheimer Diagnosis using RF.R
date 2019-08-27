d1a155c8ba8d2a29ded94bf5fc6061802c4d64f8
RohanShetty <shettyrohan641@yahoo.com>
2019-08-27 21:14:51 +0530

    second one

--- Alzheimer Diagnosis using RF.R
+++ Alzheimer Diagnosis using RF.R
@@ -1,1 +1,18 @@
 
+#load all the required libraries
+
+library(Hmisc)
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(PerformanceAnalytics)
+library(randomForest)
+library(rpart)
+library(rpart.plot)
+library(caret)
+library(pROC)
+
+ad_data <- read.csv('')
+head(ad_data)
+str(ad_data)
+summary(ad_data)

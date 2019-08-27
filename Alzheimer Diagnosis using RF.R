00e2194749d76a2b88a947c3bb6665b5e2a26b3c
RohanShetty <shettyrohan641@yahoo.com>
2019-08-27 21:19:01 +0530

    FOURTH ONE

--- Alzheimer Diagnosis using RF.R
+++ Alzheimer Diagnosis using RF.R
@@ -23,3 +23,41 @@ ad_data <- na.omit(ad_data)
 
 chart.Correlation(select(ad_data, Age, EDUC, SES, MMSE, CDR, eTIV, nWBV, ASF ), histogram = T, main = 'Correlation using PerformanceAnalytics')
 
+
+
+ggplot(ad_data) +
+  aes(x = CDR, y = Age, fill = Group) +
+  geom_boxplot() +
+  scale_fill_hue() +
+  theme_minimal()
+str(ad_data)
+summary(ad_data)
+
+
+#\\\\\\\\----------DATA VISUALIZATION-------------
+
+#CDR vs SES
+ggplot(ad_data) +
+  aes(x = CDR, y = SES) +
+  geom_boxplot(fill = "#0c4c8a") +
+  theme_minimal()
+
+#CDR vs eTIV
+ggplot(ad_data) +
+  aes(x = CDR, y = eTIV) +
+  geom_boxplot(fill = "#0c4c8a") +
+  theme_minimal()
+
+#CDR vs nWBV
+ggplot(ad_data) +
+  aes(x = CDR, y = nWBV) +
+  geom_boxplot(fill = "#0c4c8a") +
+  theme_minimal()
+
+#CDR va ASF
+ggplot(ad_data) +
+  aes(x = CDR, y = ASF) +
+  geom_boxplot(fill = "#0c4c8a") +
+  theme_minimal()
+
+
